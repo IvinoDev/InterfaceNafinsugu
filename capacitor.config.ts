f0181e7d3adb82e0d960593fbc7a903689450364
IvinoDev <kayantao@protonmail.com>
2023-02-24 14:12:32 +0000

    les fichiers de l'app mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.nafinsugu',
+  appName: 'nafinsugu',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
